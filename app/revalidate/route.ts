cb69a32c50bf738951b72a3180ff72fd67563e16
aliasghari0020 <aasghari606@gmail.com>
2026-03-19 07:25:40 +0330

    fix: add revalidate tag

--- app/revalidate/route.ts
+++ app/revalidate/route.ts
@@ -1,4 +1,4 @@
-import {revalidatePath} from 'next/cache';
+import {revalidateTag} from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({ message: 'Missing tag param' }, { status: 400 });
     }
 
-    revalidatePath('/', 'page')
+    revalidateTag(tag , secret);
 
         return NextResponse.json({ revalidated: true, now: Date.now(), tag });
 }
