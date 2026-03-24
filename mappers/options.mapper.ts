18781ab5e48b1d6b4146c954e86d8401e320d1ff
aliasghari0020 <aasghari606@gmail.com>
2026-03-24 09:56:33 +0330

    fix: optional type swiper (123)

--- mappers/options.mapper.ts
+++ mappers/options.mapper.ts
@@ -1,30 +1,31 @@
-// mappers/options.mapper.ts
-import { HomeACF } from '@/types/wordperess';
+import { WPOptions } from '@/types/wordperess';
 
-export function mapGlobalOptions(data?: HomeACF) {
-    if (!data) return {}; // return empty object if no data
+export function mapGlobalOptions(data?: WPOptions) {
+    if (!data) return {};
 
-    const acf = data;
+    const acf = data.acf; // <--- حتماً اینجا acf رو جدا کنیم
 
     return {
         hero: {
-            primaryImage: acf.hero_primary_image ?? null,
-            secondaryImage: acf.hero_secondary_image ?? null,
-            backgroundImage: acf.background_image ?? null,
+            primaryImage: acf?.hero_section_images?.hero_primary_image ?? null,
+            secondaryImage: acf?.hero_section_images?.hero_secondary_image ?? null,
+            backgroundImage: acf?.background_image ?? null,
         },
 
-        sliderImage: acf.slider_section_image ?? null,
+        sliderImage: acf?.slider_section_image ?? null,
 
-        comments: acf.comment_field?.map((item) => ({
-            description: item.description ?? '',
-            author: item.the_author ?? '',
-            job: item.author_job ?? '',
-            stars: Number(item.the_star ?? 0),
-        })) ?? [],
+        comments:
+            acf?.comment_field?.map((item) => ({
+                description: item.description ?? '',
+                author: item.the_author ?? '',
+                job: item.author_job ?? '',
+                stars: Number(item.the_star ?? 0),
+            })) ?? [],
 
-        faq: acf.faq?.map((item) => ({
-            question: item.question ?? '',
-            answer: item.answer ?? '',
-        })) ?? [],
+        faq:
+            acf?.faq?.map((item) => ({
+                question: item.question ?? '',
+                answer: item.answer ?? '',
+            })) ?? [],
     };
 }
