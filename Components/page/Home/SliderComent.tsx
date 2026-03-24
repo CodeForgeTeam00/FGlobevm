39fe3933fa801dbdde55b9a8a65ddc91b659a8fa
aliasghari0020 <aasghari606@gmail.com>
2026-03-24 09:30:33 +0330

    fix: optional type swiper

--- Components/page/Home/SliderComent.tsx
+++ Components/page/Home/SliderComent.tsx
@@ -1,9 +1,21 @@
+
 "use client";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import dynamic from "next/dynamic";
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination, Autoplay } from 'swiper/modules';
 import { StarRating } from "@/Components/Ui/star-rating";
+
+const Swiper = dynamic(
+    () => import('swiper/react').then(mod => mod.Swiper),
+    { ssr: false }
+);
+
+const SwiperSlide = dynamic(
+    () => import('swiper/react').then(mod => mod.SwiperSlide),
+    { ssr: false }
+);
+
 interface CommentItem {
     description: string;
     the_author: string;
@@ -11,37 +23,19 @@ interface CommentItem {
     the_star: string;
 }
 
-export default function MySlider({ comments }: { comments: CommentItem[] }) {
+export default function MySlider({ comments = [] }: { comments?: CommentItem[] }) {
     return (
         <div className="w-full mt-4 h-full testimonial-slider">
             <Swiper
                 modules={[Pagination, Autoplay]}
                 spaceBetween={30}
                 slidesPerView={1}
-                loop={true}
-                autoplay={{
-                    delay: 4000,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                    renderBullet: (index, className) => {
-                        return `<span class="${className} 
-                                custom-bullet 
-                                !w-2 
-                                !h-2 
-                                !bg-white/20 
-                                !opacity-100 
-                                transition-all 
-                                duration-300
-                                        [&.swiper-pagination-bullet-active]:!bg-white
-                                            [&.swiper-pagination-bullet-active]:scale-125
-                                "></span>`;
-                    },
-                }}
+                loop
+                autoplay={{ delay: 4000, disableOnInteraction: false }}
+                pagination={{ clickable: true }}
                 className="pb-10"
             >
-                {comments?.map((item, index) => (
+                {comments.map((item, index) => (
                     <SwiperSlide key={index}>
                         <div className="flex flex-col mb-20">
                             <h3 className={'mb-3 text-neutral-0 text-xl font-semibold'}>
